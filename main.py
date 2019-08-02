20ac64bb84eb60d2310be178822d001a012a1d7b
Hamza Khurshid <hamzaandumar@gmail.com>
2019-08-02 17:37:45 +0200

    Improved memory usage using temp

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from bs4 import BeautifulSoup
 import json
 import datetime
 from multiprocessing.pool import ThreadPool
+import os
 
 
 def getRecords(ID,start,end):
-    print("Starting: "+ID)
-    #The dict will act as our final JSON output
-    finalDict={"entries":[]}
+    if idExists(ID):
+        print("Already Exists: "+ID,end=',')
+        return
+
+
+    print("Starting: "+ID)    
+    
     while True:
+        #The dict will act as our final JSON output
+        finalDict={"entries":[]}
+    
         start_date=start.strftime("%Y-%m-%d")
         end_date=end.strftime("%Y-%m-%d")
         site_url="https://nwis.waterdata.usgs.gov/nwis/uv?cb_00010=on&cb_00045=on&cb_00060=on&cb_00065=on&cb_00095=on&cb_00300=on&cb_00400=on&cb_63680=on&cb_72255=on&format=html&site_no="+ID+"&period=&begin_date="+start_date+"&end_date="+end_date
@@ -23,11 +31,12 @@ def getRecords(ID,start,end):
             print("ID Done: "+ID)
             break
         
-        ##creating an array to store table headers for later use as JSON keys
+        #creating an array to store table headers for later use as JSON keys
         keys=[]
         for header in headers:
             try:
                 keys.append(header.get_text().strip())
+                
                 #replacing longer key names with shorter ones
                 if str(keys[-1]).startswith("Date"):
                     keys[-1]="datetime"
@@ -52,24 +61,59 @@ def getRecords(ID,start,end):
             except:
                 pass
             
-        ##reading the table on website and writing to JSON
+        #reading the table on website and writing to JSON
         tableRows=table.findAll('tr')[1:] ##removed the first row as it contains only headers
         for tableRow in tableRows:
             finalDict["entries"].append({})
             dataCells=tableRow.findAll('td') ##finding all cells in a row
             for iterator in range(len(keys)):
                 finalDict["entries"][-1][keys[iterator]]=dataCells[iterator].get_text().replace("A\xa0","").replace("P\xa0","").replace("\xa0","") ##storing the data into dict and removing uninterpreted signs from the readings
+        
         #changing dates for next iteration
         end=start-datetime.timedelta(days=1)
         start=end-datetime.timedelta(days=jumpDays)
         #print(end_date+" to "+start_date)
-            
+        
+        #creating a temp file
+        tempFile=open('temp/'+ID+".tmp","a+")
+        tempFile.write(json.dumps(json.loads(str(finalDict).replace("'","\"")))+'-'*10) ##appending to temp file with a seperator '-'
+        tempFile.close()
+        
+    #converting temp file chunks to one data file
+    tempFile=open('temp/'+ID+".tmp","r+")
+    tempText=tempFile.read()
+    tempFile.close()
+    os.remove('temp/'+ID+".tmp") #We don't need temp file after this
+    
+    chunks=tempText.split('-'*10) #splitting chunks in the temp file
+    chunks.pop(-1) #removing last chunk as it will always be empty
+    tempFinalDict={"entries":[]}
+    for chunk in chunks: ##adding up all chunks to make one
+        chunk=json.loads(chunk)
+        tempFinalDict["entries"]+=chunk["entries"]
+    
+    #writing the final data to JSON file
     file=open(ID+".json","w+")
-    file.write(json.dumps(json.loads(str(finalDict).replace("'","\"")))) ##storing JSON after ensuring the format is in correct JSON syntax!
+    file.write(json.dumps(json.loads(str(tempFinalDict).replace("'","\"")))) ##storing JSON after ensuring the format is in correct JSON syntax!
     file.close()
 
+def idExists(ID): ##for checking if an ID has been already scrapped
+    files=os.listdir()
+    for x in range(len(files)):
+        files[x]=files[x].replace(".json","")
+    return ID in files
 
 jumpDays=120 #to query data of this much days in one request [0-120]
+numOfThreads=20 #number of threads to run at a time for multiprocessing
+
+
+#creating a temp folder for storing temporary data
+if not os.path.exists("temp"):
+    os.makedirs("temp")
+else: #emptying temp if it already exists
+    filelist = [ f for f in os.listdir("temp")]
+    for f in filelist:
+        os.remove(os.path.join("temp", f))
 
 
 ##getting list of IDs of monitoring stations from main webpage of USGS
@@ -77,7 +121,7 @@ mainObj=BeautifulSoup(urlopen("https://nwis.waterdata.usgs.gov/nwis/current/?typ
 records=mainObj.findAll('table')[1].findAll('tr',{'valign':'top'})
 all_ids=[]
 
-##running loop for all ids for scraping complete set of all states
+##running loop for all ids for scraping complete set of all stations
 for record in records:
     try:
         all_ids.append(record.find('a').get_text())
@@ -87,16 +131,14 @@ for record in records:
 
 #for ID in all_ids:
 threads=[]
-pool = ThreadPool(processes=5)
+pool = ThreadPool(processes=numOfThreads)
 
 for ID in all_ids:
     now=datetime.datetime.now()
     end=now-datetime.timedelta(days=1)
     start=now-datetime.timedelta(days=jumpDays)
     #getRecords(ID,start,end)
-    threads.append(pool.apply_async(getRecords, (ID,start,end,))) #tuple of args for foo
+    threads.append(pool.apply_async(getRecords, (ID,start,end,)))
 
-#print('closing')
 pool.close()
-#print('joining')
 pool.join()
